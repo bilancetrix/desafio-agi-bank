57dd7785077eae56d7bd04aad58963d96a26e597
DaviAnd <bilancetrix@gmail.com>
2024-07-31 22:56:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# cypress-agi-bank-automation
+
+Sample project for a challenge testing required.
+
+## Pre-requirements
+It is required to have Node.js and npm installed to run this project.
+
+> I used versions v20.11.0 and 10.2.4 of Node.js and npm, respectively. I suggest you use the same or later versions.
+
+## Installation
+Run 'npm install' (or 'npm i' for the short version) to install the dev dependencies. In this case is the: npm install cypress --save-dev
+
+## Tests
+You can run the tests simulating a desktop or mobile viewport
+
+### Desktop
+Run 'npm test' (or 'npm t' for the short version) to run the test in headless mode on a desktop viewport.
+
+Or, run 'npm run cy:open' to open #press in interactive mode on a desktop viewport.
---
+++ cypress/e2e/pesquisa_artigo.cy.js
@@ -0,0 +1,35 @@
+describe('Pesquise um artigo', function() {
+
+  //Carrega o link do blog
+  beforeEach(function() {
+    cy.visit('https://blogdoagi.com.br/')
+  })
+
+  //Faz pesquisa de conteúdo disponível no blog
+  it('Deve procurar artigos com o termo "Investimentos"', function() {
+    cy.get('.site-header-above-section-right > .ast-builder-layout-element')
+      .click()
+    cy.get('#search-field')
+      .type('Investimentos{enter}')
+    cy.url()
+      .should('include', 's=Investimentos')
+    cy.get('.page-title > span')
+      .should('exist', 'contain.text', 'Investimentos')
+    cy.get('#post-6313 > .ast-post-format- > .post-content > .entry-title')
+      .should('exist')
+  });
+
+  //Faz pesquisa de conteúdo indisponível no blog
+  it('Deve procurar artigos com o termo "TermoAleatorioInexistente"', function() {
+    cy.get('.site-header-above-section-right > .ast-builder-layout-element')
+      .click()
+    cy.get('#search-field')
+      .type('TermoAleatorioInexistente{enter}')
+    cy.url()
+      .should('include', 's=TermoAleatorioInexistente')
+    cy.get('.page-title > span')
+      .should('exist', 'contain.text', 'TermoAleatorioInexistente')
+    cy.get('.page-content > p')
+      .should('contain.text', 'Lamentamos, mas nada foi encontrado para sua pesquisa, tente novamente com outras palavras.')
+  });
+});
